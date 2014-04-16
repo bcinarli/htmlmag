64b5c60ccd0294b88915d7f1ced0f950f85dc11a
Bilal Çınarlı <bcinarli@gmail.com>
2014-04-16 11:03:28 +0300

    page header meta informations

--- app/pages/includes/header.php
+++ app/pages/includes/header.php
@@ -15,6 +15,19 @@
 
 	<link rel="stylesheet" href="<?php echo url::styles('styles.css'); ?>" />
 	<link rel="bookmark icon" href="<?php echo url::images('favicon.png'); ?>" type="image/x-icon" />
+	<link rel="apple-touch-icon" href="<?php echo url::images('apple-touch-icon.png'); ?>">
+
+	<link rel="canonical" href="http://htmlmag.com<?php echo url::getUrl(); ?>" />
+	<link rel="author" href="https://plus.google.com/116168797582640048599/posts">
+
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="HTML Mag" />
+	<meta property="og:description" content="HTML Mag is a website dedicated for HTML, CSS and JavaScript." />
+	<meta property="og:url" content="http://htmlmag.com/" />
+	<meta property="og:site_name" content="HTML Mag" />
+	<meta property="article:publisher" content="https://www.facebook.com/htmlmag" />
+	<meta property="og:image" content="http://htmlmag.com/app/assets/images/logo@2x.png" />
 
 	<?php if(html::$externalCSS != ''): ?>
 	<link rel="stylesheet" href="<?php echo html::$externalCSS; ?>" />
