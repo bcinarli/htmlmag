8b293f496859760d212c5119d9a126a3eaa8a34a
Bilal Çınarlı <bcinarli@gmail.com>
2014-04-22 01:38:38 +0300

    New articles
    An Introduction to CSS Pre-Processors: SASS, LESS and Stylus

--- app/settings/articles.php
+++ app/settings/articles.php
@@ -3,44 +3,51 @@
 $articles = array();
 
 $articles[] = array(
-	'title' => 'Future of Web Design 2014 - Part 3',
-	'slug' => 'fowd-2014-part3',
-	'date' => '2014-04-16 23:25:45',
-	'excerpt' => 'Yeni fikirler edindiğim, webin farklı kısımları için geliştirilen farklı yaklaşımları takip ettiğim konferansın son gününü daha çok Rising Stars Track konuşmalarında geçirdim. Bu konuşmalar boyunca arayüz yazılımları ve tasarım ile kullanılabilirliğe yeni bakış açılarını değerlendirdik.'
+    'title'   => 'An Introduction to CSS Pre-Processors; SASS, LESS, and Stylus',
+    'slug'    => 'an-introduction-to-css-preprocessors-sass-less-stylus',
+    'date'    => '2014-04-21 23:41:20',
+    'excerpt' => 'CSS is primitive and incomplete. Need some extra features for building maintainable systems. Pre-processors extend CSS with variables, operators, interpolations, functions, mixins and many more other usable assets.'
 );
 
 $articles[] = array(
-	'title' => 'Future of Web Design 2014 - Part 2',
-	'slug' => 'fowd-2014-part2',
-	'date' => '2014-04-15 23:32:20',
-	'excerpt' => 'Future of Web Design konferansının 2. günü ve konuşmaların ilk günü keyifli öğretici konuşmalarla geçti. Ama günün aklımda kalan sözü Paul Boag\'ın sosyalleşme ve network imkanını üst düzeyde tutmak için "Go talk to a stranger" lafı oldu.'
+    'title'   => 'Future of Web Design 2014 - Part 3',
+    'slug'    => 'fowd-2014-part3',
+    'date'    => '2014-04-16 23:25:45',
+    'excerpt' => 'Yeni fikirler edindiğim, webin farklı kısımları için geliştirilen farklı yaklaşımları takip ettiğim konferansın son gününü daha çok Rising Stars Track konuşmalarında geçirdim. Bu konuşmalar boyunca arayüz yazılımları ve tasarım ile kullanılabilirliğe yeni bakış açılarını değerlendirdik.'
 );
 
 $articles[] = array(
-	'title' => 'Future of Web Design 2014 - Part 1',
-	'slug' => 'fowd-2014-part1',
-	'date' => '2014-04-14 11:59:10',
-	'excerpt' => 'Future Insights\'ın düzenlediği Future of Web Design konferansı 7-9 Nisan tarihleri arasında Londra\'da yapıldı. İlk günü workshoplardan oluşan konferansın ikinci ve üçüncü gününde güncel web tasarımlarından ve modern frontend uygulamalarından örneklerin sunulduğu, tartışıldığı konuşmalar ile dolu dolu geçti.'
+    'title'   => 'Future of Web Design 2014 - Part 2',
+    'slug'    => 'fowd-2014-part2',
+    'date'    => '2014-04-15 23:32:20',
+    'excerpt' => 'Future of Web Design konferansının 2. günü ve konuşmaların ilk günü keyifli öğretici konuşmalarla geçti. Ama günün aklımda kalan sözü Paul Boag\'ın sosyalleşme ve network imkanını üst düzeyde tutmak için "Go talk to a stranger" lafı oldu.'
 );
 
 $articles[] = array(
-	'title' => 'So what is HTML Mag?',
-	'slug' => 'so-what-is-htmlmag',
-	'date' => '2014-04-13 14:36:30',
-	'excerpt' => 'After a longtime residing in the dusts of history, I finally started writing for HTML Mag.'
+    'title'   => 'Future of Web Design 2014 - Part 1',
+    'slug'    => 'fowd-2014-part1',
+    'date'    => '2014-04-14 11:59:10',
+    'excerpt' => 'Future Insights\'ın düzenlediği Future of Web Design konferansı 7-9 Nisan tarihleri arasında Londra\'da yapıldı. İlk günü workshoplardan oluşan konferansın ikinci ve üçüncü gününde güncel web tasarımlarından ve modern frontend uygulamalarından örneklerin sunulduğu, tartışıldığı konuşmalar ile dolu dolu geçti.'
 );
 
 $articles[] = array(
-	'title' => 'Google Android Logo with pure html and css',
-	'slug' => 'google-android-logo-with-pure-html-and-css',
-	'date' => '2010-10-18',
-	'excerpt' => 'While building this Google Android Logo, I\'ve used pure HTML and CSS with new CSS3 border-radius and transform properties. Probably the visitors who are using Internet Explorer could not see the real shape of the final result. So for better results, use Firefox, Safari, Chrome or Opera'
+    'title'   => 'So what is HTML Mag?',
+    'slug'    => 'so-what-is-htmlmag',
+    'date'    => '2014-04-13 14:36:30',
+    'excerpt' => 'After a longtime residing in the dusts of history, I finally started writing for HTML Mag.'
 );
 
 $articles[] = array(
-	'title' => 'phpDesigner syntax highlighters with dark background',
-	'slug' => 'phpdesigner-syntax-highlighters-with-dark-background',
-	'date' => '2009-10-12',
-	'excerpt' => 'phpDesigner is one of best php IDE I\'ve worked with. Recently, its default white editor background become a pain for my eyes, so I decided to change its editor background to black.');
+    'title'   => 'Google Android Logo with pure html and css',
+    'slug'    => 'google-android-logo-with-pure-html-and-css',
+    'date'    => '2010-10-18',
+    'excerpt' => 'While building this Google Android Logo, I\'ve used pure HTML and CSS with new CSS3 border-radius and transform properties. Probably the visitors who are using Internet Explorer could not see the real shape of the final result. So for better results, use Firefox, Safari, Chrome or Opera'
+);
+
+$articles[] = array(
+    'title'   => 'phpDesigner syntax highlighters with dark background',
+    'slug'    => 'phpdesigner-syntax-highlighters-with-dark-background',
+    'date'    => '2009-10-12',
+    'excerpt' => 'phpDesigner is one of best php IDE I\'ve worked with. Recently, its default white editor background become a pain for my eyes, so I decided to change its editor background to black.');
 
 $GLOBALS['articles'] = $articles;
