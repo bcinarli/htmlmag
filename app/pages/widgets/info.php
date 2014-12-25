a7d8c5429fa5d261470ac71cc0f845f4e5372c3d
Bilal Çınarlı <bcinarli@gmail.com>
2014-12-25 14:12:23 +0200

    info update

--- app/pages/widgets/info.php
+++ app/pages/widgets/info.php
@@ -7,5 +7,8 @@
 ?>
 
 <div class="widget info-widget">
-	<p>Html Mag is a website dedicated for <strong>HTML</strong>, <strong>CSS</strong> and <strong>JavaScript</strong> subjects. Most of the contents are derived from the experience and daily workings of <a href="http://bcinarli.com"><strong>Bilal Cinarli</strong></a> who is a <span title="I know there is no such thing :), company policies"><strong>UX Developer*</strong></span> <a href="http://twitter.com/turkcell"><strong>@Turkcell</strong></a> and expert <strong>Frontend Architect</strong>, working in website industry since <strong>2002</strong>.</p>
+	<p>Html Mag is a website dedicated for <strong>HTML</strong>, <strong>CSS</strong> and <strong>JavaScript</strong> subjects.</p>
+	<h3>Who is Behind Html Mag?</h3>
+	<p>Most of the contents are derived from the experience and daily workings of <a href="http://bcinarli.com"><strong>Bilal Cinarli</strong></a> who is a <span title="I know there is no such thing :), company policies"><strong>UX Developer*</strong></span> <a href="http://twitter.com/turkcell"><strong>@Turkcell</strong></a> and expert <strong>Frontend Architect</strong>, working in website industry since <strong>2002</strong></p>
+	<p>Also <a href="https://twitter.com/nesrinkalender"><strong>Nesrin Kalender</strong></a>, who is working as a Front-end Developer at <strong>Rafineri</strong>, helps finding content and organizing the Html Mag Events.</p>
 </div>
