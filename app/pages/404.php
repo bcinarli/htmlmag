62b631e0d7f15d4f6f2e1e997a769612065b5b53
Bilal Çınarlı <bcinarli@gmail.com>
2015-02-26 20:43:43 +0200

    Update: 404 page

--- app/pages/404.php
+++ app/pages/404.php
@@ -10,13 +10,13 @@ html::$class = 'homepage';
 ?>
 <?php tools::inc('includes/header'); ?>
 	<div id="main" class="main-content">
-		<h1 class="page-title">The page, you've looked, could not found.</h1>
-		<p>Please check your url, or try another page listed below</p>
+		<h1 class="page-title">Aradığınız sayfayı bulamadık :(</h1>
+		<p>Belki yanlış bir linke basmış ya da kaldırılmış bir sayfaya denk gelmiş olabilirsiniz. Dilerseniz, aşağıdaki yazı listemizden ilginizi çeken birine bakabilirsiniz.</p>
 		<ul class="article-list">
 		<?php foreach($GLOBALS['articles'] as $article): ?>
 			<li class="article">
 				<a href="<?php echo url::make_url('article/' . $article['slug']); ?>"><?php echo $article['title']; ?></a>
-				<span class="date"><?php echo $article['date']; ?></span>
+				<span class="date"><?php echo tools::formatLocaleDate($article['date']); ?></span>
 			</li>
 		<?php endforeach; ?>
 		</ul>
