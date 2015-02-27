39bfba4eb102aa20d8d02a6a21654ecde938e888
Bilal Çınarlı <bcinarli@gmail.com>
2015-02-27 18:07:53 +0200

    New: Homepage event banner & promo section

---
+++ app/pages/widgets/promo.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Bilal Cinarli
+ * @link http://bcinarli.com
+ **/
+
+?>
+<div class="widget promo-widget">
+    <a href="<?php echo url::make_url('etkinlik/web-mobil-arayuz-mimarisi'); ?>"><img src="<?php echo url::base_url('content/2015/arayuz-mimarisi.png'); ?>" title="Web ve Mobil Arayüz Mimarisi Çalıştayı" /></a>
+</div>
