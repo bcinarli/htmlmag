a63475afc0d1032cd3b0352223902477708a9c9d
Bilal Çınarlı <bcinarli@gmail.com>
2015-03-01 14:07:26 +0200

    New: Bulletin subscription

---
+++ app/pages/widgets/bulletin.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Bilal Cinarli
+ * @link http://bcinarli.com
+ **/
+?>
+<div class="widget bulletin-widget">
+    <form action="//htmlmag.us4.list-manage.com/subscribe/post?u=c1a9b9c888fd249baa851b615&amp;id=bfc0be8210" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+        <div class="form-elements">
+            <h3 class="widget-title"><i class="icon-mail"></i> HTML Magazin Bülten</h3>
+            <label for="mc-email">Güncel haberler, etkinlik ve buluşma duyurularını yaptığımız, her ayın 3. Salı günü gönderilen bültenimize hemen abone olun!</label>
+            <div class="fields">
+                <input type="email" name="EMAIL" id="mc-email" class="email one-whole" placeholder="e-posta adresi" required />
+                <div style="position: absolute; left: -5000px">
+                    <input type="text" name="b_c1a9b9c888fd249baa851b615_bfc0be8210" tabindex="-1" value="">
+                </div>
+                <button name="subscribe" class="primary-action button">Abone Ol</button>
+            </div>
+        </div>
+    </form>
+</div>
