9a93b2a66bc14439b5123de13e75cb2090b83873
Bilal Çınarlı <bcinarli@gmail.com>
2015-04-20 08:56:50 +0300

    New: UXAlive

--- app/pages/widgets/externals.php
+++ app/pages/widgets/externals.php
@@ -8,6 +8,10 @@
     <h3 class="widget-title">Dışarıda Neler Oluyor?</h3>
 
     <ol class="external-contents">
+        <li class="external-contents">
+            <a href="<?php echo url::make_url('article/uxalive'); ?>">UX Alive! - Smart Responsive Design Patterns & Front-End Techniques</a>
+            <p>Bölgenin en büyük UX konferansında, Vitaly Friedman'ın atölyesi için kayıt yaptırabilirsiniz.</p>
+        </li>
         <li class="external-content">
             <a href="<?php echo url::make_url('article/mobil-uygulama-dunyasinda-ben-de-varim'); ?>">Mobil Uygulama Dünyasında Ben de Varım</a>
             <p>Bu yıl ikincisi gerçekleştirilen <strong>indir.com</strong> Mobil Uygulama Ödülleri 2015 yarışması için katılım süreci başladı.</p>
