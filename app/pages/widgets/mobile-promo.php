0dfb685e922c7393e0b58deb0aeb894d0cebceca
Bilal Çınarlı <bcinarli@gmail.com>
2015-05-11 20:34:08 +0300

    New: Mobile conference cover

---
+++ app/pages/widgets/mobile-promo.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Bilal Cinarli
+ * @link http://bcinarli.com
+ **/
+
+?>
+<div class="widget mobile-promo-widget" style="border-color: #1da8e3;">
+    <a href="<?php echo url::make_url('etkinlik/the-frontiers-mini'); ?>"><img src="<?php echo url::base_url('content/2015/frontiers-mobile-cover.png'); ?>" title="The Frontiers:Mini Arayüz Konferansı" /></a>
+</div>
