18dc959b1c3b6f0cffae6cc4e1fe9f6bc885fee5
Bilal Çınarlı <bcinarli@gmail.com>
2015-06-12 20:55:31 +0300

    About update

--- app/pages/widgets/info.php
+++ app/pages/widgets/info.php
@@ -10,5 +10,6 @@
     <p>HTML Magazin sitesi, HTML, CSS ve JavaScript konularında güncel ve araştırma öncelikli içerikler sunmaya adanmıştır. Hazırlanan yazılan Türkçe ve İngilizce karışık olarak sunulmaktadır.</p>
 	<h3>Biz Kimiz?</h3>
     <p>İçeriklerin büyük kısmı, 2001'den beri web teknolojileri ile geliştirme yapan, arayüz geliştirme alanında <strong>Frontend Architect</strong> olarak uzmanlaşmış ve şuan <a href="http://twitter.com/turkcell"><strong>@Turkcell</strong></a>'de <strong>Senior UX Developer</strong> olarak çalışan ve  <a href="http://bcinarli.com"><strong>Bilal Çınarlı</strong></a>'nın günlük deneyimleri ve araştırmalarından derlenmektedir.</p>
+    <p>İletişim, genel planlama ve içerik düzenlemeleri kısa sürede yapılıp büyük ses getiren projelerin uzmanı <a href="http://sirz.at">Şirzat Aytaç</a> tarafından yapılmaktadır.</p>
     <p>İçerik koordinasyonu ve HTML Magazin etkinliklerinin planları ise, <strong>Rafineri</strong>'de <strong>Front-end Developer</strong> olarak çalışan <a href="https://twitter.com/nesrinkalender"><strong>Nesrin Kalender</strong></a> tarafından yapılmaktadır.</p>
 </div>
