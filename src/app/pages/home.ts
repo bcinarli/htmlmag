9f7109b30c97c8bb330fdd22208b720c478d6932
Bilal Cinarli <bcinarli@gmail.com>
2023-02-28 13:14:59 +0100

    Disqus comments

--- src/app/pages/home.ts
+++ src/app/pages/home.ts
@@ -2,9 +2,14 @@ import { html, TypicaComponent } from '../../lib/typica';
 import articles from './home.json';
 import Summary from '../components/summary';
 import { SummaryType } from '../components/summary';
+import DisqusCount from '../components/disqus-count';
 
 const Home: TypicaComponent = () => {
   const articleSummaries = articles.map((article: SummaryType) => Summary(article)).join('');
+
+  document.addEventListener('typicaDOMUpdated', () => {
+    DisqusCount();
+  });
 
   return html`<div class="articles">${articleSummaries}</div> `;
 };
